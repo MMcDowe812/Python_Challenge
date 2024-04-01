aa2225cb4487e43c82bbb35240c2eaddad975478
MMcDowell812 <mmcdowell812@google.com>
2024-04-01 18:43:29 -0500

    updated total

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -8,20 +8,59 @@ import csv
 # Start Here
 # Read CSV File
 
-Finance_file_Path="PyBank\Resources\Finance_data.csv"
+file_Path="PyBank\Resources\Finance_data.csv"
     #Renamed file due to \b causing an error.
 total_Months = 0
-
-# open the file
-with open(Finance_file_Path) as Finance_Path:
-    csv_file=csv.reader(Finance_Path)
+total_PL = 0
+month=[]
+prof_loss=[]
+with open(file_Path) as Finance_Path:
+    csv_file = csv.reader(Finance_Path)
     next(csv_file)  #Skips a row in the file (first row = header row)
-    # Read a row in the file
-for row in csv_file:
-    print(row)
+    # Calculate the total months
+    for row in csv_file:
+        total_Months+=1
+        date = row[0]
+        money = row[1] 
+        if money not in prof_loss:
+            prof_loss.append(int(money))
+            total_PL = sum(prof_loss)
+            print(month)       
+            
+        
+   
+
+
+            
+    
+    # total profit and loss
+    #for row in prof_loss:
+        
+
+    
+        
+
 
 # Print to screen
+print("Financial Analysis")
+print('-------------------------')
+print(f'Total Months: {total_Months}')
+print(f'Total: ${total_PL}')
+#print(f'Average Change{}')
+#print(f'Greatest Increase in Profits: {}')
+#print(f'Greatest Decrease in profits: {}')
 
 # Print to File
+
+
+
+# Examples
+# Financial Analysis
+# ----------------------------
+# Total Months: 86
+# Total: $22564198
+# Average Change: $-8311.11
+# Greatest Increase in Profits: Aug-16 ($1862002)
+# Greatest Decrease in Profits: Feb-14 ($-1825558)
 
 # End Here
